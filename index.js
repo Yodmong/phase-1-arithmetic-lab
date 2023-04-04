1db850c84b1369af966fde0bf768446ed41c27ad
tidaraty <frankpook1978@gmail.com>
2023-04-04 15:27:00 -0700

    commpleted

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2; 
+    
+const random = 2
+Math.floor(Math.random() * 3) + 1;
 
-// Write your code here
+const num3 = 16
+const num4 = 6
+const mod = num3%num4;
+console.log(mod);
+
+const max = 20
+Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,);
